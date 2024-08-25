ee7fcdcf61505b03df564a62f0619e17d872fa58
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-25 22:52:21 +0000

    Sección 2: Número aleatorio criptográfico atmosférico

--- 01-cripto-random/src/App.tsx
+++ 01-cripto-random/src/App.tsx
@@ -1,11 +1,34 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
-function App() {
+export const App = () => {
+  const [number, setNumber] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState();
+  const [refreshToken, setRefreshToken] = useState(0);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    fetch(
+      "https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new"
+    )
+      .then((resp) => resp.json())
+      .then((data) => setNumber(data))
+      .catch((error) => setError(error))
+      .finally(() => setIsLoading(false));
+  }, [refreshToken]);
+
   return (
     <>
-      <h1>Hola Mundo</h1>
+      <h1>{isLoading ? "Cargando" : `Número: ${number}`}</h1>
+      <div>{error}</div>
+      <button
+        onClick={() => setRefreshToken(refreshToken + 1)}
+        disabled={isLoading}
+      >
+        Nuevo número
+      </button>
     </>
   );
-}
-
-export default App;
+};
