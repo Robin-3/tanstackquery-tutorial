350e8457367763d2011f6672c8ab8c25bcdbc748
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-25 23:28:53 +0000

    Sección 2: Manejo de errores

--- 01-cripto-random/src/App.tsx
+++ 01-cripto-random/src/App.tsx
@@ -1,8 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import "./App.css";
-import { RandomNumber } from "./components/RandomNumber";
+// import { RandomNumber } from "./components/RandomNumber";
 
 const getCriptoNumber = async (): Promise<number> => {
+  throw "No se pudo obtener el número";
+
   const resp = await fetch(
     "https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new"
   ).then((resp) => resp.json());
@@ -20,13 +22,14 @@ export const App = () => {
   } = useQuery({
     queryKey: ["randomNumber"],
     queryFn: getCriptoNumber,
-    staleTime: 1000 * 5 // 5seg
+    refetchOnWindowFocus: false,
+    retry: 1
   });
 
   return (
     <>
       <h1>{isLoading ? "Cargando" : `Número: ${number}`}</h1>
-      <RandomNumber />
+      {/* <RandomNumber /> */}
       <div>{JSON.stringify(error)}</div>
       <button onClick={() => refetch()} disabled={isFetching}>
         Nuevo número
