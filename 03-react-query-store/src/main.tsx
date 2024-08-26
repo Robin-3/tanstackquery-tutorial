5963ed80a4c09b9bf3114a88a5f1c36f106103a9
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-26 19:18:52 +0000

    Sección 8: Configurar TanStack Query

--- 03-react-query-store/src/main.tsx
+++ 03-react-query-store/src/main.tsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { NextUIProvider } from "@nextui-org/react";
-import { RouterProvider } from 'react-router-dom';
-import { router } from './router/router.tsx';
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/router.tsx";
+import "./index.css";
+import { TanStackProvider } from "./plugins/TanStackProvider.tsx";
 
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <NextUIProvider>
-      <main className="dark text-foreground bg-background">
-        <RouterProvider router={ router } />
-      </main>
-    </NextUIProvider>
-  </React.StrictMode>,
-)
+    <TanStackProvider>
+      <NextUIProvider>
+        <main className="dark text-foreground bg-background">
+          <RouterProvider router={router} />
+        </main>
+      </NextUIProvider>
+    </TanStackProvider>
+  </React.StrictMode>
+);
