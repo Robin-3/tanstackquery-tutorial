df563b01497a31995b485801310d69b985b83699
rojaslopezrobinson727 <rojaslopezrobinson727@gmail.com>
2024-08-26 12:14:26 +0000

    Sección 3: useLabels + useQuery

--- 02-tanstack-query-issues/src/issues/components/LabelPicker.tsx
+++ 02-tanstack-query-issues/src/issues/components/LabelPicker.tsx
@@ -1,22 +1,21 @@
-import { useQuery } from "@tanstack/react-query";
-import { getLabels } from "../actions";
+import { LoadingSpinner } from "../../shared";
+import { useLabels } from "../hooks";
 
 export const LabelPicker = () => {
-  const labelsQuery = useQuery({
-    queryKey: ["labels"],
-    queryFn: getLabels
-  });
+  const { labelsQuery } = useLabels();
 
   if (labelsQuery.isLoading)
     return (
-      <div className="flex justify-center items-center h-52">Espere...</div>
+      <div className="flex justify-center items-center h-52">
+        <LoadingSpinner />
+      </div>
     );
 
   return (
     <div className="flex flex-wrap gap-2 justify-center">
       {labelsQuery.data?.map((label) => (
         <span
-          className="px-2 py-1 rounded-full text-xs font-semibold hover:bg-slate-800 cursor-pointer text-white"
+          className="animate-fadeIn px-2 py-1 rounded-full text-xs font-semibold hover:bg-slate-800 cursor-pointer text-white"
           style={{ border: `1px solid #${label.color}` }}
         >
           {label.name}
